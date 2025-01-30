7f2c2ccc6ce05602011266c1ac4da4e2e45e3843
Juan Guzmán <juanjcardona13@gmail.com>
2025-01-30 06:45:44 -0500

    Add Iconify plugin and Vuetify configuration for custom icons

---
+++ plugins/iconify.ts
@@ -0,0 +1,66 @@
+// @unocss-include
+import { h } from "vue";
+import type { IconSet, IconProps } from "vuetify";
+import { Icon } from "@iconify/vue";
+
+const iconify: (set: string) => IconSet = (set) => ({
+  component: (props: IconProps) =>
+    h(Icon, {
+      ...props,
+      icon: `${set}:${props.icon}`,
+      disabled: props.disabled,
+    }),
+});
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook("vuetify:configuration", ({ vuetifyOptions }) => {
+    vuetifyOptions.icons = {
+      defaultSet: "heroicons",
+      sets: {
+        heroicons: iconify("heroicons"),
+        tabler: iconify("tabler"),
+        'simple-icons': iconify("simple-icons"),
+      },
+      aliases: {
+        collapse: "i-mdi:chevron-up",
+        complete: "i-mdi:check",
+        cancel: "i-mdi:close-circle",
+        close: "i-mdi:close",
+        delete: "i-mdi:close-circle",
+        // delete (e.g. v-chip close)
+        clear: "i-mdi:close-circle",
+        success: "i-mdi:check-circle",
+        info: "i-mdi:information",
+        warning: "i-mdi:alert-circle",
+        error: "i-mdi:close-circle",
+        prev: "i-mdi:chevron-left",
+        next: "i-mdi:chevron-right",
+        checkboxOn: "i-mdi:checkbox-marked",
+        checkboxOff: "i-mdi:checkbox-blank-outline",
+        checkboxIndeterminate: "i-mdi:minus-box",
+        delimiter: "i-mdi:circle",
+        // for carousel
+        sortAsc: "i-mdi:arrow-up",
+        sortDesc: "i-mdi:arrow-down",
+        expand: "i-mdi:chevron-down",
+        menu: "i-mdi:menu",
+        subgroup: "i-mdi:menu-down",
+        dropdown: "i-mdi:menu-down",
+        radioOn: "i-mdi:radiobox-marked",
+        radioOff: "i-mdi:radiobox-blank",
+        edit: "i-mdi:pencil",
+        ratingEmpty: "i-mdi:star-outline",
+        ratingFull: "i-mdi:star",
+        ratingHalf: "i-mdi:star-half-full",
+        loading: "i-mdi:cached",
+        first: "i-mdi:page-first",
+        last: "i-mdi:page-last",
+        unfold: "i-mdi:unfold-more-horizontal",
+        file: "i-mdi:paperclip",
+        plus: "i-mdi:plus",
+        minus: "i-mdi:minus",
+        calendar: "i-mdi:calendar",
+      },
+    };
+  });
+});
